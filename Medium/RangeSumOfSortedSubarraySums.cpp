d6b689090cd9239681321d60ef46e79beb082dd8
Joel <95717805+messiel12pr@users.noreply.github.com>
2022-09-29 22:16:17 -0400

    Create RangeSumOfSortedSubarraySums.cpp

---
+++ Medium/RangeSumOfSortedSubarraySums.cpp
@@ -0,0 +1,41 @@
+/**********************************************************
+ *
+ *  1508. Range Sum of Sorted Subarray Sums solution c++
+ *
+ *  Joel M. Gonzalez Rodriguez
+ *
+*/*********************************************************
+
+int rangeSum(vector<int> &nums, int n, int left, int right)
+{
+    vector<int> arr;
+    long int ans, l, r;
+    ans = l = r = 0;
+
+    while (l < nums.size())
+    {
+
+        if (r < nums.size())
+        {
+            ans += nums[r];
+            arr.push_back(ans);
+            r++;
+        }
+
+        else
+        {
+            ans = 0;
+            l++;
+            r = l;
+        }
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for (int i = left - 1; i < right; i++)
+    {
+        ans += arr[i];
+    }
+
+    return ans % 1000000007;
+}
