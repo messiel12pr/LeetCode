42992429e38de76237980ef9a5b8c509d45ee252
joelm12pr <joelm12pr@gmail.com>
2023-04-24 21:36:25 -0400

    Problem 1544 solved

---
+++ Easy/MakeTheStringGreat.py
@@ -0,0 +1,23 @@
+"""
+
+    1544. Make The String Great solution
+
+"""
+
+class Solution:
+    def makeGood(self, s: str) -> str:
+        l = list(s)
+        i = 0
+        
+        while i<len(l)-1:
+            if l[i] != l[i+1] and l[i].lower() == l[i+1].lower():
+                del l[i]
+                del l[i]
+                if i != 0:
+                    i -= 1
+                continue
+
+            i += 1
+
+        s = ''.join(l)
+        return s
