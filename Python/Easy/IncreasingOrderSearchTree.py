5009e2e7c2dacea2ded4b97950177306c127dc99
joelm12pr <joelm12pr@gmail.com>
2023-04-25 20:47:28 -0400

    Moving files and adding solved problem 897

---
+++ Python/Easy/IncreasingOrderSearchTree.py
@@ -0,0 +1,26 @@
+"""
+
+    897. Increasing Order Search Tree
+
+"""
+
+def inOrder(r: TreeNode, s: List):
+    if r == None:
+        return
+
+    inOrder(r.left, s)
+    r.left = None
+    s.append(r)
+    inOrder(r.right, s)
+
+class Solution:
+
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        s = []
+        inOrder(root, s)
+        root = s[0]
+
+        for i in range(len(s)-1):
+            s[i].right = s[i+1]
+
+        return root
