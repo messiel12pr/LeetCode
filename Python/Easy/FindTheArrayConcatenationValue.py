bcb6367672280e64ee65f37471bca72e2c563930
joelm12pr <joelm12pr@gmail.com>
2023-04-26 21:04:55 -0400

    Solved problem 2562 leetcode

---
+++ Python/Easy/FindTheArrayConcatenationValue.py
@@ -0,0 +1,27 @@
+'''
+    2562. Find the Array Concatenation Value 
+
+    Solution
+'''
+
+class Solution:
+    def findTheArrayConcVal(self, nums: List[int]) -> int:
+        l = sum = 0
+        r = len(nums)-1
+        s = ''
+
+        while l < r:
+            s = str(nums[l]) + str(nums[r])
+            sum += int(s)
+
+            del nums[r]
+            del nums[l]
+
+            s = ''
+            l = 0
+            r = len(nums)-1
+            
+        if len(nums) == 1:
+            sum += nums[0]
+
+        return sum
