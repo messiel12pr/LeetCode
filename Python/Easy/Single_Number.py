e75be37bce9447405fe2cf82a08da085331eafd5
Joel M. Gonzalez <joelm12pr@gmail.com>
2024-06-22 00:09:49 -0400

    Solved lc problem #136

---
+++ Python/Easy/Single_Number.py
@@ -0,0 +1,12 @@
+'''
+
+    136. Single Number
+
+'''
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
